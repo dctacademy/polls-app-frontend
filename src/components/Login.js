51b35beb94fb964374509c2f55afb8fbed968a1e
aniruddha <ani@dctacademy.com>
2023-10-10 13:31:17 +0530

    login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,55 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
 function Login() {
     const location = useLocation() 
+    const navigate = useNavigate()
     // const message = location.state ? location.state.message : ''
     const message = location.state?.message 
     // const message = location.state.message
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('') 
+    const [serverErrors, setServerErrors] = useState([])
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const formData = {
+            email,
+            password
+        }
+        try {
+            const response = await axios.post('http://localhost:3090/auth/login', formData) 
+            localStorage.setItem('token', response.data.token)
+            navigate('/')
+        } catch(e) {
+            setServerErrors(e.response.data.errors)
+        }
+    }
+
     return (
         <div>
             { message && <b> { message } </b>}
-            <h2>Login Component</h2>
+            <form onSubmit={handleSubmit}>
+                <h2>Login</h2>
+                {serverErrors.length > 0 && (
+                    <div>
+                        <h3>These errors prohibitted the form from being saved: </h3>
+                        <ul>
+                            {serverErrors.map((ele, i) => {
+                                return <li key={i}>{ele.msg}</li>
+                            })}
+                        </ul>
+                    </div>
+                )}
+                <label>
+                    Enter email <br />
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+                </label> <br />
+                <label>
+                    Enter password <br />
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                </label> <br />
+                <input type="submit" />
+            </form>
         </div>
     )
 }
